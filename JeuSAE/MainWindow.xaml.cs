bcb0e18db6a441ef8ed682f7bc8f0e5fe450d406
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-01-09 17:53:27 +0100

    super

--- JeuSAE/MainWindow.xaml.cs
+++ JeuSAE/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,38 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace JeuSAE
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour jeu.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        ImageBrush background_01 = new ImageBrush();
-        ImageBrush btn_solo = new ImageBrush();
-        ImageBrush btn_1vs1 = new ImageBrush();
-        ImageBrush btn_parametre = new ImageBrush();
         public MainWindow()
         {
-
-
             InitializeComponent();
-
-            background_01.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/zombie_decor.jpg"));
-            zombie_decor.Fill = background_01;
-
-            btn_solo.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_solo.png"));
-            solo.Background = btn_solo;
-
-            btn_1vs1.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_1vs1.png"));
-            _1vs1.Background = btn_1vs1;
-
-            btn_parametre.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_parametre.png"));
-            parametre.Background = btn_parametre;
-
         }
     }
 }
