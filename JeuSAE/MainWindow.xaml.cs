4a6cdb26ce084348c81ab33071cff8e29dcf5584
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-01-09 16:52:41 +0100

    hub

--- JeuSAE/MainWindow.xaml.cs
+++ JeuSAE/MainWindow.xaml.cs
@@ -40,8 +40,8 @@ namespace JeuSAE
             btn_1vs1.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_1vs1.png"));
             _1vs1.Background = btn_1vs1;
 
-            //btn_parametre.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_parametre.png"));
-            //parametre.Background = btn_parametre;
+            btn_parametre.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/bouton_parametre.png"));
+            parametre.Background = btn_parametre;
 
         }
     }
