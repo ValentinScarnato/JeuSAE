430c38807de27ed20b763a8dfaa39ea5cd1dd4b7
ValentinScarnato <Valentin.Scarnato@etu.univ-savoie.fr>
2024-01-10 14:20:37 +0100

    caca

--- JeuSAE/MainWindow.xaml.cs
+++ JeuSAE/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace JeuSAE
 {
@@ -20,12 +21,16 @@ namespace JeuSAE
     public partial class MainWindow : Window
     {
         private bool gauche, droite, haut, bas = false;
+        int vitesse = 1, vie = 100, munitions = 10, kills = 0;
+        string position = "haut";
+
 
         ImageBrush iconeMunition = new ImageBrush();
         ImageBrush iconeVie = new ImageBrush();
         ImageBrush joueur_ = new ImageBrush();
         ImageBrush iconeCrane = new ImageBrush();
-        
+        ImageBrush zombar = new ImageBrush();
+
 
         public MainWindow()
         {
@@ -39,8 +44,9 @@ namespace JeuSAE
             iconeMunition.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/munition.png"));
             icone_munition.Fill = iconeMunition;
             iconeVie.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/coeurs.png"));
-            icone_vie.Fill = iconeVie;
-
+            icone_vie.Fill = iconeVie; 
+            zombar.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/idle0000.png"));
+            Genere_Zombies(zombar);
         }
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
@@ -65,10 +71,26 @@ namespace JeuSAE
             else if (e.Key == Key.Down)
                 bas = false;
         }
+        public void Genere_Zombies(ImageBrush zombiiee)
+        {
+            int nbr, i=0;
+            Random n = new Random();
+            nbr = n.Next(10, 20);
+            if (i < nbr)
+            InitializeComponent();
+            Random rnd = new Random();
+            ZombieCanvas.Fill = zombiiee;
+            Rectangle zombie = new Rectangle();
+            //zombiiee.ImageSource = 
+            //zombiiee.Left = rnd.Next(0, 900);
+            //zombiiee.Top = rnd.Next(0, 800);
+
+
+        }
+
+
+
+
 
     }
-
-
-
-
 }
