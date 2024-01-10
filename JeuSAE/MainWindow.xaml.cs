f9a5feb7b26accad386af354c0467ff960811b59
Enzo Benech <enzo.benech@etu.univ-smb.fr>
2024-01-10 12:12:58 +0100

    fix

--- JeuSAE/MainWindow.xaml.cs
+++ JeuSAE/MainWindow.xaml.cs
@@ -19,85 +19,18 @@ namespace JeuSAE
     /// </summary>
     public partial class MainWindow : Window
     {
-        /*private String ChoixFenetre;
-
-        public String choixFenetre
-        {
-            get { return ChoixFenetre; }
-            set
-            {
-                if (value != "Menu") ;
-                throw new ArgumentException("Fenetre incorrecte");
-                ChoixFenetre = value;
-
-            }
-
-
-        }
-        public MainWindow() { 
-            WindowJeu menu = new WindowJeu();
-            menu.ShowDialog();
-         
-        }
         private bool gauche, droite, haut, bas = false;
-        private void Window_KeyDown(object sender, KeyEventArgs e)
 
-        }*/
-        ImageBrush joueur_ = new ImageBrush();
-        ImageBrush iconeCrane = new ImageBrush();
         ImageBrush iconeMunition = new ImageBrush();
         ImageBrush iconeVie = new ImageBrush();
-
-
+        ImageBrush joueur_ = new ImageBrush();
+        ImageBrush iconeCrane = new ImageBrush();
+        
 
         public MainWindow()
         {
-            if (e.Key == Key.Left)
-                gauche = true;
-            else if (e.Key == Key.Right)
-                droite = true;
-            else if (e.Key == Key.Up)
-                haut = true;
-            else if (e.Key == Key.Down)
-                bas = true;
-
-        }
-        private void Window_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Left)
-                gauche = false;
-            else if (e.Key == Key.Right)
-                droite = false;
-            else if (e.Key == Key.Up)
-                haut = false;
-            else if (e.Key == Key.Down)
-                bas = false;
-
-
-        }
-        /*InitializeComponent();
-        this.ChoixFenetre = "Menu";
-        while (true)
-        {
-            switch (ChoixFenetre)
-            {
-                case "Menu":
-                {
-                        WindowJeu menu = new WindowJeu();
-                        menu.fenetre = this;
-                        menu.ShowDialog();
-                        break;
-                    }
-                case "niv1":
-                    {
-                        FenetreJeu niv1 = new FenetreJeu();
-                        niv1.fenetre = this;
-                        niv1.ShowDialog();
-                        break;
-                    }
-            }*/
-
-
+            WindowJeu menu = new WindowJeu();
+            menu.ShowDialog();
             InitializeComponent();
             joueur_.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/joueur_shotgun.png"));
             joueur.Fill = joueur_;
@@ -108,30 +41,38 @@ namespace JeuSAE
             iconeVie.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Image/coeurs.png"));
             icone_vie.Fill = iconeVie;
 
-
-            /*this.ChoixFenetre = "Menu";
-            while (true)
-            {
-                switch (ChoixFenetre)
-                {
-                    case "Menu":
-                    {
-                            WindowJeu menu = new WindowJeu();
-                            menu.fenetre = this;
-                            menu.ShowDialog();
-                            break;
-                        }
-                    case "niv1":
-                        {
-                            FenetreJeu niv1 = new FenetreJeu();
-                            niv1.fenetre = this;
-                            niv1.ShowDialog();
-                            break;
-                        }
-                }*/
         }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+                gauche = true;
+            else if (e.Key == Key.Right)
+                droite = true;
+            else if (e.Key == Key.Up)
+                haut = true;
+            else if (e.Key == Key.Down)
+                bas = true;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+                gauche = false;
+            else if (e.Key == Key.Right)
+                droite = false;
+            else if (e.Key == Key.Up)
+                haut = false;
+            else if (e.Key == Key.Down)
+                bas = false;
+        }
+
     }
-} 
+
+
+
+
+}
+  
 
     
         
